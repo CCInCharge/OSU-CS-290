06d1406af8130d20a8915368ca58c87b54cd3cde
CCInCharge <charles.l.chen.clc@gmail.com>
2017-05-25 20:15:29 -0700

    Completed API How To

--- howto/scripts.js
+++ howto/scripts.js
@@ -1,2 +1,21 @@
+function searchAuthor() {
+  var api_key = "ae353dbedd394dbda571ffda02387b07";
+  var url = "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?";
+	var author = $("#author").val();
+  url += $.param({
+      'api-key': api_key,
+      'author': author
+    });
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).done(function(result) {
+    console.log(result);
+  }).fail(function(err) {
+    throw err;
+  });
+}
+
 $(document).ready(function() {
+  $("#search").click(searchAuthor);
 });
